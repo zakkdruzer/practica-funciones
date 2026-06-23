ce946db356530ebb66b97eb7b519843f0eadcf67
zakkdruzer <zakkdruzer@gmail.com>
2026-06-22 23:44:27 -0300

    feat: reto 2 - funcion multiplicar

--- reto-1.js
+++ reto-1.js
@@ -1,5 +1,6 @@
 console.log("%cReto 1 · Tu primera función", "font-weight: bold; color: green; font-size: 15px;");
 console.log("")
+
 let nombre1= "María"
 let nombre2= "José"
 
---
+++ reto-2.js
@@ -0,0 +1,17 @@
+console.log("%cReto 2 · La calculadora reutilizable", "font-weight: bold; color: green; font-size: 15px;");
+console.log("")
+
+let numero1 = 6
+let numero2 = 7
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+let resultado = multiplicar(6, 7);
+
+console.log("Resultado:", resultado);
+
+console.log("El doble:", resultado * 2);
+
+console.log("")
