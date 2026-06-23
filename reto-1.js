ca74c4836d0ded3cea6b5f61f1ebd6b4f50d2aa3
zakkdruzer <zakkdruzer@gmail.com>
2026-06-22 23:38:18 -0300

    feat: reto 1 - completado

---
+++ reto-1.js
@@ -0,0 +1,13 @@
+console.log("%cReto 1 · Tu primera función", "font-weight: bold; color: green; font-size: 15px;");
+console.log("")
+let nombre1= "María"
+let nombre2= "José"
+
+function presentar(nombre){
+    return ("Hola mi nombre es "+ nombre);
+}
+
+console.log(presentar(nombre1));
+console.log(presentar(nombre2));
+
+console.log("")
